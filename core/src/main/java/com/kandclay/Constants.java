7c7b9f71a68400b9c1a4c7dde6857e37161a3c53
Javier Francisco Dibo Gomez <jfdg0002@red.ujaen.es>
2024-06-09 15:07:09 +0200

    Fixed animation Constants

--- core/src/main/java/com/kandclay/Constants.java
+++ core/src/main/java/com/kandclay/Constants.java
@@ -2,9 +2,9 @@ package com.kandclay;
 
 public class Constants {
     public static class Animation {
-        public static final int NUM_COLS = 4; // Number of columns in the sprite sheet
-        public static final int NUM_ROWS = 2; // Number of rows in the sprite sheet
-        public static final float FRAME_DURATION = 0.1f; // Duration of each frame
+        public static final int NUM_COLS = 28; // Number of columns in the sprite sheet
+        public static final int NUM_ROWS = 28; // Number of rows in the sprite sheet
+        public static final float FRAME_DURATION = 5f / 100f; // Duration of each frame
     }
 }
 
